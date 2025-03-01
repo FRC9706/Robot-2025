1fde545cd49b5b420ed9d2c5151e35bf98696198
Iskand <tajfentabi@gmail.com>
2025-03-01 13:36:11 -0800

    Fixed conversion factors according to gear ratio

--- src/main/java/frc/robot/ArmConstants.java
+++ src/main/java/frc/robot/ArmConstants.java
@@ -5,8 +5,8 @@ public class ArmConstants {
     public static final double kArmPgain = 0.00002;
     public static final double kArmIgain = 0.0000001;
     public static final double kArmDgain = 0.0001;
-    public static final double kArmPositionConversionFactor = 1000;
-    public static final double kArmVelocityConversionFactor = 1000;
+    public static final double kArmPositionConversionFactor = 64/12;
+    public static final double kArmVelocityConversionFactor = 64/12;
     public static final boolean kInverted = true;
     public static final double kArmPosition1 = 0;
     public static final double kArmPosition2 = 0.15;
