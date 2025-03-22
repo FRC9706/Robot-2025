18dab95edce5f7ad271b17979765c1c12882fb9e
Iskand <tajfentabi@gmail.com>
2025-03-22 09:17:35 -0700

    Saving PID to arm constants for no reason

--- src/main/java/frc/robot/ArmConstants.java
+++ src/main/java/frc/robot/ArmConstants.java
@@ -2,14 +2,13 @@ package frc.robot;
 
 public class ArmConstants {
     public static final int kArmMotorID = 13;
-    public static final double kArmPgain = 0.0001;
+    public static final double kArmPgain = 0.065;
     public static final double kArmIgain = 0;
-    public static final double kArmDgain = 0;
+    public static final double kArmDgain = 0.01;
+    public static final double kArmFFGain = 0.000003;
     public static final double kArmPositionConversionFactor = 64/12;
     public static final double kArmVelocityConversionFactor = 64/12;
-    public static final boolean kInverted = false;
-    public static final double kArmPosition1 = 10;
-    public static final double kArmPosition2 = 5;
-    public static final double kMinOutput = -0.1;
-    public static final double kMaxOutput = 0.1;
+    public static final boolean kInverted = true;
+    public static final double kArmPosition1 = 0;
+    public static final double kArmPosition2 = 0.15;
 }
