44d3e841164bdeb0ff8b1f22cbf51e8fa7e42db3
MindcraftRobotics <MindcraftProgramming@gmail.com>
2025-10-10 21:20:43 -0700

    fix

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -6,13 +6,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Parameters;
 
@@ -20,20 +13,7 @@ public class Climb extends SubsystemBase {
     public static final TalonFX climbMotor = new TalonFX(Parameters.kClimbMotorID);
     public static final TalonFX climbMotor2 = new TalonFX(Parameters.kClimbMotorID2);
 
-    public static final TalonFX climbMotor = new TalonFX(Parameters.kClimbMotorID);
-    public static final TalonFX climbMotor2 = new TalonFX(Parameters.kClimbMotorID2);
-
     private boolean isRunning = false;
-    public static Climb mInstance = null;
-    public static Climb getInstance() {
-        if (mInstance == null) {
-            mInstance = new Climb();
-        }
-        return mInstance;
-    }
-    
-
-
     public static Climb mInstance = null;
     public static Climb getInstance() {
         if (mInstance == null) {
@@ -94,7 +74,7 @@ public class Climb extends SubsystemBase {
         climbMotor.getConfigurator().apply(talonFXConfigs);
 
         climbMotor2.getConfigurator().apply(talonFXConfigs2);
-        climbMotor2.setControl(new Follower(Parameters.kClimbMotorID, false)); // true was ben 10 iq fr
+        climbMotor2.setControl(new Follower(Parameters.kClimbMotorID, false)); // true was ben 10 iq
 
         climbMotor.setPosition(0);
         climbMotor2.setPosition(0);
@@ -102,23 +82,7 @@ public class Climb extends SubsystemBase {
 
     final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
 
-    final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
-
     public void climb() {
-            if (isRunning) {
-                climbMotor.set(0);
-            } else {
-                climbMotor.set(1);
-            }
-            isRunning = !isRunning; // even better
-    }
-
-    public double armDegsToMotorDegs(double input) {
-        return input*5.091;
-    }
-
-    public void goToRot(double rot) {
-        climbMotor.setControl(m_request.withPosition(rot));
             if (isRunning) {
                 climbMotor.set(0);
             } else {
