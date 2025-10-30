0fb59eb0f16611d75220e23ecffbd29c1ad61510
MindcraftRobotics <MindcraftProgramming@gmail.com>
2025-10-30 16:04:33 -0700

    Created resetPosFeildCentric()

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -28,6 +28,7 @@ import frc.robot.DetectorConstants;
 import frc.robot.Limelight;
 import frc.robot.LimelightHelpers;
 import frc.robot.Parameters;
+import frc.robot.Robot;
 //import frc.robot.LimelightHelpers;
 // import com.ctre.phoenix6.hardware.Pigeon2;
 import frc.robot.Parameters.CTRESwerveDrivetrain;
@@ -114,6 +115,11 @@ public class Swerve extends CTRESwerveDrivetrain implements Subsystem {
             DogLog.log("Swerve/Modules/" + moduleNames[i] + "/TurnTemperature", getModule(i).getSteerMotor().getDeviceTemp().getValueAsDouble());
         }
 
+    }
+
+    public static void resetPosFeildCentric() {
+        Robot.drivetrain.resetRotation(Rotation2d.k180deg);
+        Robot.drivetrain.resetRotation(Rotation2d.kZero);
     }
 
     private final PIDController xController = new PIDController(Parameters.kDrivePgain, 
