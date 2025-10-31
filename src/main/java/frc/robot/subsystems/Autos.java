c875f9f49f85ef51c97ce9dae43bc6a8d4eecb59
nav-util <navka.si5787@vusd.us>
2025-10-30 22:11:23 -0700

    Fixed Autoroutines!!! Should work now

--- src/main/java/frc/robot/subsystems/Autos.java
+++ src/main/java/frc/robot/subsystems/Autos.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -39,7 +40,7 @@ public class Autos extends SubsystemBase {
             drivetrain.getState().Pose, 
             drivetrain::resetPose, 
             drivetrain::followTrajectory, 
-            false, 
+            true, 
             drivetrain);
         }
     
